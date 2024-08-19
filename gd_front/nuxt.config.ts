58b950825b3c16024c133f82fab2968bb4e4ce39
Vlad <shirker2006@gmail.com>
2024-08-19 09:42:44 -0700

    up

--- gd_front/nuxt.config.ts
+++ gd_front/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   ssr: false,
   runtimeConfig: {
     public: {
-      apiBaseUrl: 'http://127.0.0.1:8000', // Replace with your Laravel backend URL
+      apiBaseUrl: process.env.API_BASE_URL || 'http://127.0.0.1:8000', // Replace with your Laravel backend URL
     },
   },
   css: [
