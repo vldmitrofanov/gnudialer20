4fb3a087575f7d249ac97cc17e8be46a855b0ef0
Vlad <shirker2006@gmail.com>
2024-09-13 17:05:54 -0700

    wip

--- gd_front/pages/admin/reports/sales.vue
+++ gd_front/pages/admin/reports/sales.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>Reports here</div>
-    </template>
-    <script setup>
-    definePageMeta({
-      layout: 'admin', 
-      middleware: 'auth-admin',
-    })
-    </script>
+  <a-breadcrumb>
+    <a-breadcrumb-item>
+      <NuxtLink to="/admin">Admin</NuxtLink>
+    </a-breadcrumb-item>
+    <a-breadcrumb-item>
+      <NuxtLink to="/admin/reports">Reports</NuxtLink>
+    </a-breadcrumb-item>
+    <a-breadcrumb-item>Sales Reports</a-breadcrumb-item>
+  </a-breadcrumb>
+  <div>Sales Reports here</div>
+</template>
+<script setup>
+definePageMeta({
+  layout: 'admin',
+  middleware: 'auth-admin',
+})
+</script>
