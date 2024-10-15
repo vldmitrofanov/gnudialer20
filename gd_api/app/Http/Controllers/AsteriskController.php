e3c97449d8bcea4213ecfa1756d6fc0d127fadfd
Vlad <shirker2006@gmail.com>
2024-10-15 16:52:48 -0700

    up

--- gd_api/app/Http/Controllers/AsteriskController.php
+++ gd_api/app/Http/Controllers/AsteriskController.php
@@ -170,7 +170,7 @@ class AsteriskController extends Controller
         $serverId = $request->server_id;
         $this->amiService->setServer($serverId);
         $command = "Action: Originate\r\n";
-        $command .= "Channel: " . rtrim($threeWay->trunk, '/'). '/' . $threeWay->extension . "\r\n";
+        $command .= "Channel: " . $threeWay->trunk . $threeWay->extension . "\r\n";
         //$command .= "Channel: Local/{$threeWay->extension}@{$threeWay->context}\r\n";
         $command .= "Exten: $threeWay->extension\r\n";  // The third party's SIP channel
         $command .= "Context: $threeWay->context\r\n";  // The dialplan context
