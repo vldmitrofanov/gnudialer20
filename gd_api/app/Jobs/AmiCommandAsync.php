0ef82ef3143c5ead92018179a7958110a6cea357
Vlad <shirker2006@gmail.com>
2024-10-25 14:07:16 -0700

    up

--- gd_api/app/Jobs/AmiCommandAsync.php
+++ gd_api/app/Jobs/AmiCommandAsync.php
@@ -4,6 +4,7 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use App\Services\AsteriskAMIService;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -15,7 +16,7 @@ class AmiCommandAsync implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct($amiHandle, $payload, $endLine)
+    public function __construct(AsteriskAMIService $amiHandle, $payload, $endLine)
     {
         $this->amiHandle = $amiHandle;
         $this->payload = $payload;
